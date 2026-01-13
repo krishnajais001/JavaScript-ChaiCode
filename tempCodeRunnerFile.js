11b5ad8a43f5d29141eae7c9ec1519d99e3183cd
Krishna Kumar <154787698+krishnajais001@users.noreply.github.com>
2026-01-13 23:03:03 +0000

    conversionOf Datatype

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log(typeof score)
+// console.log(typeof score)
+
+// let valueInNumber = Number(score); // gurnate its a number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
